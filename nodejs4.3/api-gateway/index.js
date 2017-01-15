d7956d69a16a4b261ea13fe654b2ccc66e7ff70a
Tomek Kopczuk <tkopczuk@gmail.com>
2017-01-15 19:49:48 +0100

    Updated example.

--- nodejs4.3/api-gateway/index.js
+++ nodejs4.3/api-gateway/index.js
@@ -1,14 +1,12 @@
 // Just a test lambda, run with:
-// docker run -v "$PWD":/var/task lambci/lambda:api-gateway
+// docker run --rm -v "$PWD":/var/task -p 80:8000 lambci/lambda:api-gateway
 
-exports.handler = function(event, context, cb) {
-  context.success({
+exports.handler = function(event, context) {
+  context.succeed({
     statusCode: 200,
     body: JSON.stringify({status: 'ok'}),
     headers: {
       'Content-Type': 'application/json'
     }
   });
-
-  return cb();
 }
